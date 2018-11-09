9d4956b4c512c23cc5140316aae64e4bd9df3865
Davide <dbarasti@gmail.com>
2018-11-09 16:00:19 +0100

    primo commit per branch windows

--- routes.js
+++ routes.js
@@ -3,6 +3,13 @@ var bodyParser = require("body-parser");
 var session = require("express-session");
 var router = express.Router();
 
+// Get the adodb module
+var ADODB = require('database-js-adodb');
+
+//const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=SagrationData2018.accdb;');
+var connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=SagrationData2018.accdb;Persist Security Info=False;');
+
+
 
 router.use(function(req, res, next){
 	res.locals.errors = req.flash("error");
@@ -10,11 +17,29 @@ router.use(function(req, res, next){
 	next();
 });
 
+// Query the DB
+app.get('/testquery', (req, res) => {
+ connection
+  .query('SELECT * FROM Items WHERE prezzo>9')
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2));
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  res.send('tutti i dati della query sono disponibili sul log')
+})
+
 //homepage
-router.get("/", function(req,res){
+router.get("/", function(req, res){
 	console.log("Questa è la pagina di benveunto");
 	res.send("landing page");
 })
+
+router.get("/stats/:statType", function(req, res, next){
+
+})
+
 
 
 //404 page
